4827d3b6fe31df524b23004a7b740d4bae13d861
Luiz Oliveira Montedonio <montedonio.luiz@gmail.com>
2021-08-19 15:45:50 -0300

    Update and Delete user endpoints

--- src/controllers/User.ts
+++ src/controllers/User.ts
@@ -5,16 +5,6 @@ import { Request, Response } from 'express';
 import User from "../schemas/User";
 
 class UserController {
-  public async getAll(req: Request, res: Response): Promise<Response> {
-    const user = await User.find()
-      .catch(err => {
-        console.log("Getall user error: ", err)
-        return res.status(500).json({ message: "Error while fetching users" })
-      });
-
-    return res.json(user);
-  }
-
   public async create(req: Request, res: Response): Promise<Response> {
     const user = await User.create(req.body)
       .catch(err => {
@@ -23,6 +13,36 @@ class UserController {
       });
 
     return res.status(201).json(user);
+  }
+
+  public async read(req: Request, res: Response): Promise<Response> {
+    const users = await User.find()
+      .catch(err => {
+        console.log("Getall user error: ", err)
+        return res.status(500).json({ message: "Error while fetching users" })
+      });
+
+    return res.json(users);
+  }
+
+  public async update(req: Request, res: Response): Promise<Response> {
+    const user = await User.findByIdAndUpdate(req.params.userId, req.body, { new: true })
+      .catch(err => {
+        console.log("Update user error: ", err)
+        return res.status(500).json({ message: "Error while updating the user" })
+      });
+
+    return res.json(user);
+  }
+
+  public async delete(req: Request, res: Response): Promise<Response> {
+    const user = await User.findByIdAndDelete(req.params.userId)
+      .catch(err => {
+        console.log("Delete user error: ", err)
+        return res.status(500).json({ message: "Error while removing the user" })
+      });
+
+    return res.send();
   }
 }
 
--- src/routes/User.ts
+++ src/routes/User.ts
@@ -4,7 +4,9 @@ import UserController from '../controllers/User';
 
 const routes = Router();
 
-routes.get('/', UserController.getAll);
+routes.get('/', UserController.read);
 routes.post('/', UserController.create);
+routes.put('/:userId', UserController.update);
+routes.delete('/:userId', UserController.delete);
 
 export default routes;
