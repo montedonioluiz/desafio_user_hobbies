a035d2d89466d86e01d9e48de6eeac94f38fecad
Luiz Oliveira Montedonio <montedonio.luiz@gmail.com>
2021-08-19 15:59:28 -0300

    fix user delete

--- src/controllers/User.ts
+++ src/controllers/User.ts
@@ -36,7 +36,7 @@ class UserController {
   }
 
   public async delete(req: Request, res: Response): Promise<Response> {
-    const user = await User.findByIdAndDelete(req.params.userId)
+    await User.findByIdAndDelete(req.params.userId)
       .catch(err => {
         console.log("Delete user error: ", err)
         return res.status(500).json({ message: "Error while removing the user" })
